5e64e140fb5b57436332887670d6110a8bbbfc19
Martin Randa <martin.randa@proton.me>
2022-11-30 22:56:36 +0100

    Created article_props.py module

--- testing_requests.py
+++ testing_requests.py
@@ -1,8 +1,7 @@
-import datetime
-from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+from bs4 import BeautifulSoup
 import tmc_utils.tor_initialization as ti
+import tmc_utils.article_props as article_props
 
 
 # %%
@@ -15,56 +14,16 @@ print("Actual IP:", requests.get("http://httpbin.org/ip", timeout=30).text)
 # %%
 # Dummy request
 req = tor_request.get(
-    "https://www.idnes.cz/zpravy/zahranicni/koronavirus.K466979/200",
+    "https://www.idnes.cz/zpravy/zahranicni/koronavirus.K466979/203",
     timeout=30
 )
 
 # %%
 # Parse HTML to see whether everything works
 soup = BeautifulSoup(req.text, "html.parser")
-content = soup.find("div", {"id": "list-art-count"})
-
-# Initialize empty dictionary
-article_dict = {
-    "link": [],
-    "date": [],
-    "time": [],
-    "title": [],
-    "perex": [],
-    "premium": []
-}
-
-# Find and append links, dates, time, titles, perex, and premium access
-for item in content.findAll("div", {"class": "art"}):
-    link = item.find("a", {"class": "art-link"}, href=True)["href"]
-    article_dict["link"].append(link)
-
-    date_and_time = pd.to_datetime(
-        item.find("span", {"class": "time"})["datetime"]
-    )
-    article_dict["date"].append(date_and_time.date())
-    # Articles with no specific time have 00:00:00 as the time of publication
-    if date_and_time.time() == datetime.time(0, 0):
-        article_dict["time"].append(pd.NA)
-    else:
-        article_dict["time"].append(date_and_time.time())
-
-    title = item.find("a", {"class": "art-link"}).find("h3").text.strip()
-    article_dict["title"].append(title)
-
-    # 'perex' is the lead paragraph of each article displayed in the preview
-    perex = item.find("p", {"class": "perex"})
-    # It also gives us the ability to find whether it is a premium article
-    if perex.find("a", {"class": "premlab"}) is None:
-        article_dict["perex"].append(perex.text.strip())
-        article_dict["premium"].append(False)
-    else:
-        # The word "Premium" is skipped
-        article_dict["perex"].append(perex.text[len("Premium") + 1:].strip())
-        article_dict["premium"].append(True)
 
 # Do not persist at this point. Persist cleaned text (stemmed etc.) later
-article_df = pd.DataFrame(article_dict)
+article_df = article_props.generate_article_df(soup)
 
 # %%
 ti.kill_tor_process()
---
+++ tmc_utils/article_props.py
@@ -0,0 +1,82 @@
+"""Generate a dataframe with article properties.
+
+Produces a dataframe with article details such as title, date, or link.
+Note that it is tailored to a specific HTML structure of idnes.cz.
+
+The module contains the following function:
+
+- `generate_article_df(soup_object)` - Generates a dataframe of article properties
+"""
+
+import pandas as pd
+from random import shuffle
+import datetime
+
+
+def generate_article_df(soup_object):
+    """Generate a dataframe of article properties
+
+    Args:
+        soup_object (bs4.BeautifulSoup): Parsed HTML document using BeautifulSoup
+
+    Returns:
+        pandas.core.frame.DataFrame: Dataframe with article properties
+    """
+    soup_content = soup_object.find("div", {"id": "list-art-count"})
+    # Initialize empty dictionary
+    article_dict = {
+        "link": [],
+        "date": [],
+        "time": [],
+        "title": [],
+        "perex": [],
+        "premium": [],
+        "video": []
+    }
+
+    # Find and append links, dates, time, titles, perex, and premium access
+    for item in soup_content.findAll("div", {"class": "art"}):
+        link = item.find("a", {"class": "art-link"}, href=True)["href"]
+        article_dict["link"].append(link)
+
+        date_and_time = pd.to_datetime(
+            item.find("span", {"class": "time"})["datetime"]
+        )
+        article_dict["date"].append(date_and_time.date())
+        # Articles with no specific time have 00:00:00 as the time of publication
+        if date_and_time.time() == datetime.time(0, 0):
+            article_dict["time"].append(pd.NA)
+        else:
+            article_dict["time"].append(date_and_time.time())
+
+        title = item.find("a", {"class": "art-link"}).find("h3").text.strip()
+        title_list = title.split()
+        shuffle(title_list)
+        article_dict["title"].append(title_list)
+
+        # 'perex' is the lead paragraph of each article (shortened here)
+        perex = item.find("p", {"class": "perex"})
+        # Video articles don't have perex in the preview
+        if perex is None:
+            article_dict["perex"].append(pd.NA)
+            article_dict["premium"].append(False)
+        # It also gives us the ability to find whether it is a premium article
+        elif perex.find("a", {"class": "premlab"}) is None:
+            perex_list = perex.text.strip().split()
+            shuffle(perex_list)
+            article_dict["perex"].append(perex_list)
+            article_dict["premium"].append(False)
+        else:
+            # The word "Premium" is skipped
+            perex_list = perex.text[len("Premium") + 1:].strip().split()
+            shuffle(perex_list)
+            article_dict["perex"].append(perex_list)
+            article_dict["premium"].append(True)
+        # Some articles are purely video-based
+        video = item.find("a", {"score-type": "Video"})
+        if video is not None:
+            article_dict["video"].append(True)
+        else:
+            article_dict["video"].append(False)
+
+    return pd.DataFrame(article_dict)
