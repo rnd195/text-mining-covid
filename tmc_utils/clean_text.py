b562611203ebeaeafd58cde482dbcbc141ce4529
Martin Randa <martin.randa@proton.me>
2022-12-07 21:50:36 +0100

    Implemented stemming and lemmatization

--- tmc_utils/clean_text.py
+++ tmc_utils/clean_text.py
@@ -2,49 +2,29 @@
 
 This module cleans Czech text following these steps: remove numbers, convert to
 lowercase (except for names), remove punctuation, remove double spaces, remove
-stopwords, tokenize, lemmatize, and filter out 1 letter words.
+stopwords, tokenize, stem after lemmatization, and filter out 1 letter words.
+Note that, people usually choose either stemming or lemmatization in text
+mining analyses. However, while the lemmatization tool that we use works very
+well, on its own, it doesn't seem to recognize words such as "koronavirus,"
+which is of paramount importance to our analysis. Thus, we unconventionally
+apply both techniques.
+
 
 The module contains the following functions:
 
-- `text_cleaner_cz(text_string)` - Prepares the SOCKS proxy for TOR
+- `text_cleaner_cz(text_string)` - Cleans Czech text for text mining
 """
 
 import re
 import string
 import nltk
-# from sumy.nlp.stemmers import czech
-from stop_words import get_stop_words
 import simplemma as sl
-
-
-# Lemmatization needs help with some words
-lemma_helper = {
-  "koronavirus": [
-    "koronavir",
-    "koronaviru",
-    "koronavire",
-    "koronavirem",
-    "koronaviry",
-    "koronavirů",
-    "koronavirům",
-    "koronavirech"
-  ],
-  "covid": [
-    "covid",
-    "covidu",
-    "covide",
-    "covidem",
-    "covidy",
-    "covidů",
-    "covidům",
-    "covidech"
-  ]
-}
-
+from sumy.nlp.stemmers import czech
+from stop_words import get_stop_words
 
 
 def sentence_cleaner_cz(text_string: str):
-    """Pre-process Czech text for text mining
+    """Pre-process Czech sentences for text mining
 
     Args:
         text_string (str): Text to be pre-processed
@@ -70,10 +50,11 @@ def sentence_cleaner_cz(text_string: str):
     # Tokenize words and sort alphabetically to break up meaning
     token_words = sorted(nltk.tokenize.word_tokenize(text_cleaned))
 
-    # Lemmatize instead of stemming, note that lemmatization can introduce
-    # uppercase words, e.g., germany -> Germany. We omit this feature.
+    # Use both lemmatization and stemming, see module description
     token_words_lm = [
-        sl.lemmatize(t, lang="cs").lower() for t in token_words if len(t) > 1
+        czech.stem_word(sl.lemmatize(t, lang="cs").lower())
+        for t in token_words
+        if len(t) > 1
     ]
 
     return token_words_lm
